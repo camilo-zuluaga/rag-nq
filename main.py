e2781c8020500120c7304a9541463bc81b97e91a
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-05-18 17:01:21 -0500

    feat: process document file

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from llama_index.core import (
     load_index_from_storage,
     get_response_synthesizer
 )
-from llama_index.core.node_parser import MarkdownElementNodeParser
+from llama_index.core.node_parser import MarkdownNodeParser
 from llama_index.core.prompts import PromptTemplate
 from llama_index.core.retrievers import VectorIndexRetriever
 from llama_index.core.query_engine import RetrieverQueryEngine
@@ -25,3 +25,9 @@ embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 # Global settings (still will pass embed_model as param)
 Settings.embed_model = embed_model
 Settings.llm = OpenAI(model="gpt-4o-mini")
+
+# Process the docs
+document = SimpleDirectoryReader(input_files=['data/docs.md']).load_data()
+text_splitter = MarkdownNodeParser()
+Settings.text_splitter = text_splitter
+
