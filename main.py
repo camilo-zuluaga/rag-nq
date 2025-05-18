148090ba272fd2ae58d32abeedd2fca16a59e04d
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-05-18 17:05:15 -0500

    feat: vector database

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from llama_index.core import (
     VectorStoreIndex,
     StorageContext,
     load_index_from_storage,
-    get_response_synthesizer
+    get_response_synthesizer,
 )
 from llama_index.core.node_parser import MarkdownNodeParser
 from llama_index.core.prompts import PromptTemplate
@@ -27,7 +27,25 @@ Settings.embed_model = embed_model
 Settings.llm = OpenAI(model="gpt-4o-mini")
 
 # Process the docs
-document = SimpleDirectoryReader(input_files=['data/docs.md']).load_data()
+document = SimpleDirectoryReader(input_files=["data/docs.md"]).load_data()
 text_splitter = MarkdownNodeParser()
 Settings.text_splitter = text_splitter
 
+# Vector database setup
+CHROMA_DB_PATH = "./chroma_db"
+COLLECTION = "neatqueue"
+
+db = chromadb.PersistentClient(path=CHROMA_DB_PATH)
+chroma_collection = db.get_or_create_collection(COLLECTION)
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+if len(chroma_collection.get()["ids"]) == 0:
+    index = VectorStoreIndex.from_documents(
+        document,
+        storage_context=storage_context,
+        embed_model=embed_model,
+        transformations=[text_splitter],
+    )
+else:
+    index = VectorStoreIndex.from_vector_store(vector_store, embed_model=embed_model)
