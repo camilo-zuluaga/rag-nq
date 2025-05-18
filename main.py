e7e9e66f58a8e87304866a3cf4157c11de8bad35
camilo-zuluaga <zuluagajuanca@outlook.es>
2025-05-18 16:38:13 -0500

    feat: add llama_index imports, chromadb

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hey koji")
+from dotenv import load_dotenv
+
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.openai import OpenAI
+from llama_index.core import (
+    Settings,
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+    StorageContext,
+    load_index_from_storage,
+    get_response_synthesizer
+)
+from llama_index.core.node_parser import MarkdownElementNodeParser
+from llama_index.core.prompts import PromptTemplate
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.vector_stores.chroma import ChromaVectorStore
+
+import chromadb
+
+load_dotenv()
